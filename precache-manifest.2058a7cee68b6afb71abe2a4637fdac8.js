14ee73d9e92ba0d7cfb49205c0ef42839b5f8531
McRight <yurnero017@gmail.com>
2019-09-18 13:00:40 +0300

    Updates

---
+++ precache-manifest.2058a7cee68b6afb71abe2a4637fdac8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5947e55b825dab02ce162db7673a3c2a",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "412027abf476acf8d57b",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "412027abf476acf8d57b",
+    "url": "/contactmanager/static/js/2.ab382891.chunk.js"
+  },
+  {
+    "revision": "e9195a76ff521cfe32fc",
+    "url": "/contactmanager/static/js/main.59a46acd.chunk.js"
+  },
+  {
+    "revision": "1c95488b1d464dd638b2",
+    "url": "/contactmanager/static/js/runtime~main.7d5643ef.js"
+  }
+]);
