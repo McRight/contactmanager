288b96e17d8d978b9d6839161ad85a3afd511cfd
McRight <yurnero017@gmail.com>
2019-09-18 12:55:12 +0300

    initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default props => {
+  return (
+    <div>
+      <h1 className="display-4">Contact Manager App</h1>
+      <p className="lead">Simple app to manage contacs</p>
+      <p className="text-secondary">Version 1.0.0</p>
+    </div>
+  );
+};
